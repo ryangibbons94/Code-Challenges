ddcc6b4418ccb26fd63453ded94178afd2e269cf
ryangibbons94 <96315276+ryangibbons94@users.noreply.github.com>
2022-03-17 08:51:31 -0400

    powwwwaaa of 2

---
+++ powers of 2.js
@@ -0,0 +1,11 @@
+// Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+
+function powersOfTwo(n){
+    var arry = []
+    for (let i = 0; i <= n; i++){
+      arry.push(2**i)
+    }
+    return  arry
+  }
+
+  //I tried to assign the (2**i) to a variable at first and then push it into the array but that didnt work so I looked up how to do this and it came back with this, basically. 
