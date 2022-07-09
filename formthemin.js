03223ab99f24765a488a27a09929c379e775def2
ryangibbons94 <96315276+ryangibbons94@users.noreply.github.com>
2022-07-09 11:34:27 -0400

    completed a challenege

---
+++ formthemin.js
@@ -0,0 +1,28 @@
+// Task
+// Given a list of digits, return the smallest number that could be formed from these digits, using the digits only once (ignore duplicates).
+
+// Notes:
+// Only positive integers will be passed to the function (> 0 ), no negatives or zeros.
+// Input >> Output Examples
+// minValue ({1, 3, 1})  ==> return (13)
+// Explanation:
+// (13) is the minimum number could be formed from {1, 3, 1} , Without duplications
+
+// minValue({5, 7, 5, 9, 7})  ==> return (579)
+// Explanation:
+// (579) is the minimum number could be formed from {5, 7, 5, 9, 7} , Without duplications
+
+// minValue({1, 9, 3, 1, 7, 4, 6, 6, 7}) return  ==> (134679)
+// Explanation:
+// (134679) is the minimum number could be formed from {1, 9, 3, 1, 7, 4, 6, 6, 7} , Without duplications
+
+//first i removed duplicates, then i sorted it in ascending order, then i joined the array into a string and converted it to a number
+function minValue(values) {
+  let newArr = [];
+  for (let num of values) {
+    if (newArr.indexOf(num) === -1) {
+      newArr.push(num);
+    }
+  }
+  return +newArr.sort((a, b) => a - b).join("");
+}
